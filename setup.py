271ccfaf9b5883896b38c92854364849f068a1fa
wipodev <ajwipo@gmail.com>
2024-04-25 18:28:03 -0500

    versiones especificas

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ setup(
     license='MIT',
     author='Wipodev',
     author_email='ajwipo@gmail.com',
-    packages=find_packages(exclude=('tests*', 'testing*')),
+    packages=find_packages(exclude=('tests*', 'testing*', 'test*')),
     keywords='python, captcha, speech recognition, selenium, web automation',
     long_description=open('README.md', 'r').read(),
     long_description_content_type='text/markdown',
     install_requires=[
-        'seleniumbase',
-        'pydub',
-        'SpeechRecognition',
+        'seleniumbase=4.25.4',
+        'pydub=0.25.1',
+        'SpeechRecognition=3.10.3',
     ],
 )

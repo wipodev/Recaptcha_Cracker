a10df60193660ef71b2afe8109539c12e7e2060c
wipodev <ajwipo@gmail.com>
2024-04-24 20:57:26 -0500

    modificacion menor

--- test/test_cracker.py
+++ test/test_cracker.py
@@ -7,7 +7,8 @@ def test_click_recaptcha():
     driver = Driver(uc=True)
     driver.get("https://www.google.com/recaptcha/api2/demo")
     cracker = RecaptchaCracker(driver=driver)
-    cracker.click_recaptcha(selector='//*[@id="recaptcha-demo"]/div/div/iframe')
-    input("Press Enter to continue...")
+    checked_status = cracker.click_recaptcha(selector='//*[@id="recaptcha-demo"]/div/div/iframe')
+
+    assert checked_status == True
   except Exception:
     pytest.fail('Failed to automatically resolve ReCAPTCHA.')

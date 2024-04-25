6470e5c537af522e800199de093ce7faf63f8b2b
wipodev <ajwipo@gmail.com>
2024-04-24 19:23:14 -0500

    agregada pytest al test ejecutarlo

--- test/test_cracker.py
+++ test/test_cracker.py
@@ -1,9 +1,13 @@
 from recaptcha_cracker import RecaptchaCracker
 from seleniumbase import Driver
+import pytest
 
 def test_click_recaptcha():
+  try:
     driver = Driver(uc=True)
     driver.get("https://www.google.com/recaptcha/api2/demo")
     cracker = RecaptchaCracker(driver=driver)
     cracker.click_recaptcha(selector='//*[@id="recaptcha-demo"]/div/div/iframe')
     input("Press Enter to continue...")
+  except Exception:
+    pytest.fail('Failed to automatically resolve ReCAPTCHA.')

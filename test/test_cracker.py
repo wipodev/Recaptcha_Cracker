1f9af86189d2bae5da5ce3f9216bf9e518153ddb
wipodev <ajwipo@gmail.com>
2024-04-24 23:17:27 -0500

    correccion del test

--- test/test_cracker.py
+++ test/test_cracker.py
@@ -1,14 +1,10 @@
 from recaptcha_cracker import RecaptchaCracker
 from seleniumbase import Driver
-import pytest
 
 def test_click_recaptcha():
-  try:
-    driver = Driver(uc=True)
-    driver.get("https://www.google.com/recaptcha/api2/demo")
-    cracker = RecaptchaCracker(driver=driver)
-    checked_status = cracker.click_recaptcha(selector='//*[@id="recaptcha-demo"]/div/div/iframe')
-
-    assert checked_status == True
-  except Exception:
-    pytest.fail('Failed to automatically resolve ReCAPTCHA.')
+  driver = Driver(uc=True)
+  driver.get("https://www.google.com/recaptcha/api2/demo")
+  cracker = RecaptchaCracker(driver=driver)
+  checked_status = cracker.click_recaptcha(selector='//*[@id="recaptcha-demo"]/div/div/iframe')
+  print(checked_status)
+  assert checked_status == True
